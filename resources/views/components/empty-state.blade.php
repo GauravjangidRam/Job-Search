f2f61982251d4b286800e55f9a30ae5103ce9ed8
GauravjangidRam <gaurav14jangid@gmail.com>
2026-06-16 23:20:01 +0530

    empty state

--- resources/views/components/empty-state.blade.php
+++ resources/views/components/empty-state.blade.php
@@ -3,5 +3,5 @@
 <div {{ $attributes->merge(['class' => 'flex items-center justify-center py-12 px-6']) }}>
     <p class="text-gray-500 text-center text-lg">
         {{ $message }}
-    </p>
+    </p> 
 </div>
