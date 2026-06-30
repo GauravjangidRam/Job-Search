772307fb5808cee59c80fe8d6815a2eead7bd164
GauravjangidRam <gaurav14jangid@gmail.com>
2026-06-30 20:44:10 +0530

    layouts file issue resolved

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
         <script src="https://unpkg.com/lucide@latest"></script>
         <!-- Styles / Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
+    </head> 
     <body class="font-sans antialiased bg-background text-foreground overflow-x-hidden">
         <!-- Skip Navigation Link -->
         <a href="#main-content" class="sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-50 focus:px-4 focus:py-2 focus:bg-primary focus:text-white focus:rounded-md focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2">
