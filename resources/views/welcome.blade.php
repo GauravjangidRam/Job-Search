d4cdfe4eb77e326fbd4c9b72355e307781294a3e
GauravjangidRam <gaurav14jangid@gmail.com>
2026-07-03 22:39:07 +0530

    welcome page changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -215,9 +215,8 @@
                 </div>
             </main>
         </div>
-
         @if (Route::has('login'))
             <div class="h-14.5 hidden lg:block"></div>
         @endif
     </body>
-</html>
+</html> 
