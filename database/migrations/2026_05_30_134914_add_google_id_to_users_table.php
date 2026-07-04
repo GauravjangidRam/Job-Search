740472915118d33ed2bf477ea48c943637411b45
GauravjangidRam <gaurav14jangid@gmail.com>
2026-07-04 22:40:56 +0530

    google id to user tables

--- database/migrations/2026_05_30_134914_add_google_id_to_users_table.php
+++ database/migrations/2026_05_30_134914_add_google_id_to_users_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->string('google_id')->nullable()->unique()->after('email');
             $table->string('avatar_url')->nullable()->after('avatar_path');
-        });
+        }); 
     }
 
     public function down(): void
