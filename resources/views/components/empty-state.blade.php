c2992ac6b76786dfa979014cab179e611a10b268
GauravjangidRam <gaurav14jangid@gmail.com>
2026-07-05 23:20:16 +0530

    empty state struture changes

--- resources/views/components/empty-state.blade.php
+++ resources/views/components/empty-state.blade.php
@@ -1,7 +1,8 @@
 @props(['message'])
 
+
 <div {{ $attributes->merge(['class' => 'flex items-center justify-center py-12 px-6']) }}>
     <p class="text-gray-500 text-center text-lg">
-        {{ $message }}
+        {{ $message }} 
     </p> 
-</div> 
+</div>
