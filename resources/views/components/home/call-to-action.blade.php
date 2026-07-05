dff29def8f94aa68d148455c15fc58510c4e6ad0
GauravjangidRam <gaurav14jangid@gmail.com>
2026-07-05 23:01:31 +0530

    call to action

--- resources/views/components/home/call-to-action.blade.php
+++ resources/views/components/home/call-to-action.blade.php
@@ -17,7 +17,7 @@
             <a href="{{ route('employer.register') }}" class="inline-flex items-center gap-2 border-2 border-white text-white font-semibold px-8 py-3 rounded-lg hover:bg-white/10 transition-colors focus:outline-2 focus:outline-offset-2 focus:outline-white">
                 <i data-lucide="building-2" class="w-5 h-5"></i>
                 Post a Job
-            </a>
+            </a> 
         @else
             <a href="{{ route('jobs.index') }}" class="inline-flex items-center gap-2 bg-white text-primary font-semibold px-8 py-3 rounded-lg hover:bg-white/90 transition-colors focus:outline-2 focus:outline-offset-2 focus:outline-white">
                 <i data-lucide="search" class="w-5 h-5"></i>
@@ -28,5 +28,5 @@
                 Explore Companies
             </a>
         @endif
-    </div>
-</section> 
+    </div> 
+</section>
