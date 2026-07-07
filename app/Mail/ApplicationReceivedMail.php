5935d13435ba63b699f8217ddfa04e6cbffd84e0
GauravjangidRam <gaurav14jangid@gmail.com>
2026-07-07 23:43:25 +0530

    application issue resolved

--- app/Mail/ApplicationReceivedMail.php
+++ app/Mail/ApplicationReceivedMail.php
@@ -48,9 +48,9 @@ class ApplicationReceivedMail extends Mailable
      * Get the attachments for the message.
      *
      * @return array<int, \Illuminate\Mail\Mailables\Attachment>
-     */
+     */ 
     public function attachments(): array
     {
         return [];
-    }
-} 
+    } 
+}
