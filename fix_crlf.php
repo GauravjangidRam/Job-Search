d2071db9a3701f376f6d24ac8ff8a7d7ad8d8e32
GauravjangidRam <gaurav14jangid@gmail.com>
2026-07-11 19:20:25 +0530

    502 error status code resolved

---
+++ fix_crlf.php
@@ -0,0 +1,6 @@
+<?php
+$f = 'start.sh';
+file_put_contents($f, str_replace("\r\n", "\n", file_get_contents($f)));
+$f = 'Dockerfile';
+file_put_contents($f, str_replace("\r\n", "\n", file_get_contents($f)));
+echo "Done.";
