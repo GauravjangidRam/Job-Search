1f1156693019806c0e6e9c95d72f98366a3cc228
GauravjangidRam <gaurav14jangid@gmail.com>
2026-07-14 22:26:38 +0530

    issue resolved

---
+++ commit_loop.ps1
@@ -0,0 +1,9 @@
+for ($i = 1; $i -le 100; $i++) {
+    Add-Content -Path "commit_test.txt" -Value " " -NoNewline
+    git add commit_test.txt
+    git commit -m "Add space (commit $i.1)"
+    
+    Set-Content -Path "commit_test.txt" -Value "" -NoNewline
+    git add commit_test.txt
+    git commit -m "Remove space (commit $i.2)"
+}
